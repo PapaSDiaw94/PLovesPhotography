11cc77b8b3e6b9673eb9434ef750b28f9be40fc9
Papa D <diaw.psd@gmail.com>
2024-12-28 11:00:38 -0500

    Polished bottom bar to meet expected polish. added colors for future use.

--- app/src/main/java/com/example/plovesphotography/MainActivity.kt
+++ app/src/main/java/com/example/plovesphotography/MainActivity.kt
@@ -1,44 +1,40 @@
 package com.example.plovesphotography
 
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.plovesphotography.screens.AboutScreen
 import com.example.plovesphotography.screens.CategoryScreen
 import com.example.plovesphotography.screens.FavoritesScreen
 import com.example.plovesphotography.screens.HomeScreen
+import com.example.plovesphotography.ui.theme.DarkGray
+import com.example.plovesphotography.ui.theme.DarkSalmon
 import com.example.plovesphotography.ui.theme.PLovesPhotographyTheme
+import com.example.plovesphotography.ui.theme.Valspar
 import com.exyte.animatednavbar.AnimatedNavigationBar
-import com.exyte.animatednavbar.animation.balltrajectory.Parabolic
 import com.exyte.animatednavbar.animation.balltrajectory.Teleport
-import com.exyte.animatednavbar.items.dropletbutton.DropletButton
+import com.exyte.animatednavbar.animation.indendshape.Height
+import com.exyte.animatednavbar.animation.indendshape.shapeCornerRadius
+import com.exyte.animatednavbar.items.wigglebutton.WiggleButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -89,20 +85,25 @@ fun AnimatedNavigationBar(
 ) {
     var selectedIndex by remember { mutableStateOf(0) }
 
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(70.dp) // Adjust the height here
-            .background(MaterialTheme.colorScheme.primaryContainer) // Change bar color here
-    ) {
         AnimatedNavigationBar(
+            modifier = Modifier
+                .height(85.dp),
             selectedIndex = selectedIndex,
-            barColor = MaterialTheme.colorScheme.primaryContainer, // Use a custom color
-            ballColor = MaterialTheme.colorScheme.secondary, // Adjust indicator color
-            ballAnimation = Teleport(tween(500, easing = LinearEasing))
+            barColor = Valspar, // Use a custom color
+            ballColor = DarkSalmon,
+            cornerRadius = shapeCornerRadius(25.dp),
+            ballAnimation = Teleport(tween(500, easing = LinearEasing)),
+            indentAnimation = Height(
+                indentWidth = 56.dp,
+                indentHeight = 15.dp,
+                animationSpec = tween(
+                    1000,
+                    easing = { OvershootInterpolator().getInterpolation(it) })
+            )
         ) {
             items.forEachIndexed { index, item ->
-                DropletButton(
+                WiggleButton(
+                    modifier = Modifier.fillMaxSize(),
                     isSelected = selectedIndex == index,
                     onClick = {
                         selectedIndex = index
@@ -113,8 +114,10 @@ fun AnimatedNavigationBar(
                         }
                     },
                     icon = item.icon,
+                    backgroundIcon = item.icon,
+                    outlineColor = DarkGray,
+
                 )
             }
         }
-    }
 }
--- app/src/main/java/com/example/plovesphotography/ui/theme/Color.kt
+++ app/src/main/java/com/example/plovesphotography/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val WhiteSmoke = Color( 0xFFF6EFE5)
+val DarkSalmon = Color(0xFFE09F7A)
+val DarkGray = Color(0xFF342E25)
+val Champagne = Color(0xFFF0D8B6)
+val Valspar = Color(0xFFE7D8C4)
+
+
